9f5465a0b27d7113f482638bc358f74a91c6f525
Nines <nines9@gmail.com>
2023-03-15 16:59:05 +0000

    Reduced the necessary code to achieve gadget generation.

---
+++ ysoserial/Helpers/GadgetSurrogates/ObjectDataProviderSurrogates.cs
@@ -0,0 +1,28 @@
+﻿namespace ysoserial.Helpers.SurrogateClasses
+{
+    /// <summary>
+    /// Surrogate class for bait-and-switch version of ObjectDataProvider.
+    /// </summary>
+    internal sealed class ObjectDataProviderSurrogate
+    {
+        public string MethodName { get; set; }
+        public object ObjectInstance { get; set; }
+    }
+
+    /// <summary>
+    /// Surrogate class for bait-and-switch version of ProcessStartInfo.
+    /// </summary>
+    internal sealed class ProcessStartInfoSurrogate
+    {
+        public string FileName { get; set; }
+        public string Arguments { get; set; }
+    }
+
+    /// <summary>
+    /// Surrogate class for bait-and-switch version of Process.
+    /// </summary>
+    internal sealed class ProcessSurrogate
+    {
+        public ProcessStartInfoSurrogate StartInfo { get; set; }
+    }
+}
